006d5c87e20bffbede0a2fea97bc3667939db875
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-12-05 21:45:56 +0100

    finishing the exercises

--- index.js
+++ index.js
@@ -9,26 +9,52 @@ function changeTitle() {
 
 //EX8
 function changeBackgroundColor() {
-    let background = document.querySelector("body");
+    let backgroundColor = document.querySelector("body");
     let colorInputNode = document.getElementById("new-background-color-input");
-    //let newColor = colorInputNode.value;
+    let newColor = colorInputNode.value;
 
-    background.style.changeBackgroundColor = newcolor;
+    backgroundColor.style.background = newColor;
 }
 
 //EX9
 function changeAddress() {
     let footerAddress = document.querySelector("#address");
-    let newAddress = titleInputNode.value;
+    let addressInputNode = document.getElementById("new-address-input");
+    let newAddress = addressInputNode.value;
+
     footerAddress.innerText = newAddress;
 }
 
 //EX10
 function addClass() {
-    
+    let links = document.querySelectorAll("a");
+    for (let i = 0; i < links.length; i++) {
+        links[i].classList.add("link");
+    }
 }
 
 //EX11
-
+function changeVisibility() {
+    let picturesNode = document.getElementsByClassName("cover");
+    for (let i = 0; i < picturesNode.length; i++) {
+        let pictureNode = picturesNode[i];
+        pictureNode.classList.toggle("invisible");
+    }
+}
 
 //EX12
+function changePriceColor() {
+    let pricesNode = document.getElementsByClassName('price');
+    
+    let randomBlue = Math.floor(Math.random()*(255 + 1));
+    let randomGreen = Math.floor(Math.random()*(255 + 1));
+    let randomRed = Math.floor(Math.random()*(255 + 1));
+    let randomColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+
+    //if I put all the random var in the loop, I get different colors for each price.
+    
+    for (let i = 0; i < pricesNode.length; i++) {
+        let priceNode = pricesNode[i];
+        priceNode.style.color = randomColor;
+    }
+}
