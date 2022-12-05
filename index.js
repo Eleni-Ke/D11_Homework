2e28ee0479c96d39707a18a2198b7474e295a7b8
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-12-05 17:00:57 +0100

    ex7 and 8

---
+++ index.js
@@ -0,0 +1,34 @@
+//EX7
+function changeTitle() {
+    let h1Node = document.querySelector("h1");
+    let titleInputNode = document.getElementById('new-title-input');
+    let newTitle = titleInputNode.value;
+
+    h1Node.innerText = newTitle;
+}
+
+//EX8
+function changeBackgroundColor() {
+    let background = document.querySelector("body");
+    let colorInputNode = document.getElementById("new-background-color-input");
+    //let newColor = colorInputNode.value;
+
+    background.style.changeBackgroundColor = newcolor;
+}
+
+//EX9
+function changeAddress() {
+    let footerAddress = document.querySelector("#address");
+    let newAddress = titleInputNode.value;
+    footerAddress.innerText = newAddress;
+}
+
+//EX10
+function addClass() {
+    
+}
+
+//EX11
+
+
+//EX12
